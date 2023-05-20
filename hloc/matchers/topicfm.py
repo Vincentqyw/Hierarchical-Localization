173a577be025d6e0b56ca176aa6f1a89df9452d4
Realcat <realcat@126.com>
2023-05-20 16:29:54 +0000

    update: topic-fm

--- hloc/matchers/topicfm.py
+++ hloc/matchers/topicfm.py
@@ -5,9 +5,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / '../../third_party'))
 from TopicFM.src.models.topic_fm import TopicFM as _TopicFM
-from TopicFM.src.config.default import get_cfg_defaults
-from TopicFM.src.utils.misc import lower_config
-# from TopicFM.demo import demo_utils 
+from TopicFM.src import get_model_cfg
 topicfm_path = Path(__file__).parent / '../../third_party/TopicFM'
 
 class TopicFM(BaseModel):
@@ -15,7 +13,7 @@ class TopicFM(BaseModel):
         'weights': 'outdoor',
         'match_threshold': 0.2,
         'n_sampling_topics': 4,
-        'config_path': topicfm_path / 'configs/aspan/outdoor/aspan_test.py',
+        # 'config_path': topicfm_path / 'configs/aspan/outdoor/aspan_test.py',
     }
     required_inputs = [
         'image0',
@@ -29,8 +27,8 @@ class TopicFM(BaseModel):
         weight_path =  topicfm_path / 'pretrained/model_best.ckpt'
 
         print("model config: ", _conf)
-        self.net = TopicFM(config=_conf)
-        ckpt_dict = torch.load(weight_path)
+        self.net = _TopicFM(config=_conf)
+        ckpt_dict = torch.load(weight_path, map_location='cpu')
         self.net.load_state_dict(ckpt_dict['state_dict'])
 
     def _forward(self, data):
